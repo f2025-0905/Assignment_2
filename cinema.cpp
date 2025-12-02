6c9afde354baf8dbe3cdee895b715a65413faaae
SHAFQAT ULLAH KHAN <f2025-0905@bnu.edu.pk>
2025-12-02 23:23:26 +0000

    Cinema seating system with 2-D arrays

---
+++ cinema.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int rows, columns;
+    
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    
+    cout << "Enter the number of coloumns: ";
+    cin >> columns;
+    
+    char reserveSeats [rows][columns];
+    
+    for (int i = 0; i < rows; i++){
+        for (int j = 1; j <= columns; j++){
+            reserveSeats [i][j] = 'F';
+            cout << "(" << i << "-" << j << " " << reserveSeats [i][j] <<")  ";
+        }
+    cout << "\n";
+    }
+    
+    int seatNumber;
+    cout << "Enter seat number to reserve: ";
+    cin >> seatNumber;
+    
+    reserveSeats [seatNumber/10][seatNumber%10] = 'R';
+    
+    bool runLoop = true;
+    
+    while (runLoop){
+        
+    for (int i = 0; i < rows; i++){
+        for (int j = 1; j <= columns; j++){
+            cout << "(" << i << "-" << j << " " << reserveSeats [i][j] <<")  ";
+        }
+    cout << "\n";
+    }
+    
+    
+       int option, freeSeat;
+      
+       cout << "What do you want?\n";
+       cout << "1. Reserve a seat\n";
+       cout << "2. Free a seat\n";
+       cout << "0. to exit\n";
+       cout << "Enter: ";
+       cin >> option;
+    
+        switch (option){
+            case 1: cout << "Enter seat number to reserve: ";
+                cin >> seatNumber;
+                reserveSeats [seatNumber/10][seatNumber%10] = 'R';
+            break;
+            case 2: cout << "Enter seat number to unreserve: ";
+                cin >> freeSeat;
+                reserveSeats [freeSeat/10][freeSeat%10] = 'F';
+            break;
+            case 0: runLoop = false;
+            break;
+            default: cout << "Invalid Option";
+        }
+    }
+    
+    
+    return 0;
+}
